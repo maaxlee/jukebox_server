7d7476c26d9f5eaa4ee996e9a497c4963696c231
Maxim Aliseiko <maliseiko@klika-tech.com>
2019-10-25 23:56:08 +0300

    Fix format

--- main.go
+++ main.go
@@ -6,26 +6,24 @@ import (
 	"log"
 	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 	"time"
-    "strings"
-    "os"
 )
 
-const (
-	credFile = "creds.txt"
-)
+const credFile = "creds.txt"
 
 var (
-    songsNum = 10
-    songsListMap = make(map[string][]string)
-    usersMap = make(map[string]string)
+	songsNum     = 10
+	songsListMap = make(map[string][]string)
+	usersMap     = make(map[string]string)
 )
 
 func getSong(user string) string {
 
-    log.Println(songsNum)
-    songsList := songsListMap[user]
+	log.Println(songsNum)
+	songsList := songsListMap[user]
 	if len(songsList) == 0 {
 		log.Print("List is empty, create a new list")
 		for i := 1; i < songsNum+1; i++ {
@@ -37,57 +35,57 @@ func getSong(user string) string {
 
 	ret := songsList[0]
 	songsList = songsList[1:]
-    songsListMap[user] = songsList
-    log.Println("User: " + user)
-    log.Println(songsList)
+	songsListMap[user] = songsList
+	log.Println("User: " + user)
+	log.Println(songsList)
 	return ret
 }
 
-func checkCreds (u, p string) bool {
-    if pass, ok := usersMap[u]; !ok || p != pass {
-        return false
-    }
-    return true
+func checkCreds(u, p string) bool {
+	if pass, ok := usersMap[u]; !ok || p != pass {
+		return false
+	}
+	return true
 
 }
 
 func simpleHandler(w http.ResponseWriter, r *http.Request) {
-    user, pass, ok := r.BasicAuth()
-    if !ok || !checkCreds(user, pass) {
-        w.WriteHeader(http.StatusUnauthorized)
-        fmt.Fprintln(w, "Unauthorized!")
-        return
-    }
+	user, pass, ok := r.BasicAuth()
+	if !ok || !checkCreds(user, pass) {
+		w.WriteHeader(http.StatusUnauthorized)
+		fmt.Fprintln(w, "Unauthorized!")
+		return
+	}
 	fmt.Fprintln(w, getSong(user))
 }
 
 func main() {
 
-    // Get songs number
-    if n, ok := os.LookupEnv("SONGS_NUM"); ok {
-        var err error
-        songsNum, err = strconv.Atoi(n)
-        if err != nil {
-            log.Fatal("Wrong songs number format type")
-        }
-    }
+	// Get songs number
+	if n, ok := os.LookupEnv("SONGS_NUM"); ok {
+		var err error
+		songsNum, err = strconv.Atoi(n)
+		if err != nil {
+			log.Fatal("Wrong songs number format type")
+		}
+	}
 
-    // Reading credentials file
-    log.Println("Reading DB")
-    data, err := ioutil.ReadFile(credFile)
+	// Reading credentials file
+	log.Println("Reading DB")
+	data, err := ioutil.ReadFile(credFile)
 	if err != nil {
 		log.Fatal("Could not read credentials DB")
 	}
-    strList := strings.Split(string(data), "\n")
-    for _, str := range strList {
-        if str != "" {
-            raw := strings.Split(str, ":")
-            usersMap[raw[0]] = raw[1]
-            songsListMap[raw[0]] = make([]string, 0)
-        }
-    }
+	strList := strings.Split(string(data), "\n")
+	for _, str := range strList {
+		if str != "" {
+			raw := strings.Split(str, ":")
+			usersMap[raw[0]] = raw[1]
+			songsListMap[raw[0]] = make([]string, 0)
+		}
+	}
 
-    // start server
+	// start server
 	log.Print("Starting server")
 	http.HandleFunc("/", simpleHandler)
 	http.ListenAndServe(":8080", nil)
